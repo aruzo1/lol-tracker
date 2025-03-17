6702b3ca88304974aae7d236dc73a4a8ce53a9e1
Dawid <dawid12391111@gmail.com>
2025-03-17 14:55:03 +0100

    Finish summoner search text input

--- components/summoner-search/summoner-search.tsx
+++ components/summoner-search/summoner-search.tsx
@@ -1,50 +1,66 @@
 "use client";
 
-import { Input } from "@/components/ui/input";
 import React from "react";
+import { Input } from "@/components/ui/input";
+
+function getHashCount(value: string) {
+  return (value.match(/#/g) || []).length;
+}
 
 export function SummonerSearch() {
   const [search, setSearch] = React.useState("");
   const includesHash = React.useMemo(() => search.includes("#"), [search]);
   const showHint = React.useMemo(
     () => search.length > 0 && !includesHash,
-    [search]
+    [search, includesHash]
   );
+
+  const addHash = React.useCallback((value: string) => {
+    setSearch(value.trim().replace("#", "") + " #");
+  }, []);
 
   const onSearchChange = React.useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
-      let value = e.target.value;
+      const value = e.target.value;
 
       // If there are more than one `#`, don't do anything further
-      if ((value.match(/#/g) || []).length > 1) {
+      if (getHashCount(value) > 1) {
         return;
       }
 
       // If there isn't a `#` and the user adds one, append it with a space
       if (!includesHash && value.includes("#")) {
-        setSearch(value.trim().replace("#", "") + " #");
+        addHash(value);
       } else {
         setSearch(value);
       }
     },
-    [includesHash]
+    [addHash, includesHash]
+  );
+
+  const onSearchKeyDown = React.useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === "ArrowRight" && getHashCount(search) === 0) {
+        addHash(search);
+      }
+    },
+    [search, addHash]
   );
 
   return (
     <Input asChild>
-      <label
-        htmlFor="summoner-search"
-        className="mt-6 h-12 bg-background relative"
-      >
+      <label htmlFor="summoner-search" className="mt-6 h-12 relative">
         <input
           id="summoner-search"
           name="summoner-search"
           className="w-full outline-none"
-          placeholder="Game name + #tag"
+          placeholder="Game name + #tagline"
           value={search}
           onChange={onSearchChange}
+          onKeyDown={onSearchKeyDown}
+          autoComplete="off"
         />
-        <div className="absolute flex items-center h-full top-0">
+        <div className="absolute flex items-center h-full top-0 -z-10">
           <span className="invisible whitespace-pre">{search} </span>
           <span className="text-muted-foreground select-none">
             {showHint && "#"}
