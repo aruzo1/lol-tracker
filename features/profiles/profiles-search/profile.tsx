d0dab0707dc19e9a6fca129277faf87f3988876b
Dawid <dawid12391111@gmail.com>
2025-03-18 20:51:48 +0100

    Code refactor

--- features/profiles/profiles-search/profile.tsx
+++ features/profiles/profiles-search/profile.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { getSummonerProfileIconUrl } from "@/features/summoners/urls";
+import { getSummonerProfileIconUrl } from "@/features/summoners/utils";
 import { ProfileSuggestion } from "@/features/profiles/types";
 
 export function Profile({ account, summoner }: ProfileSuggestion) {
